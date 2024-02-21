22386d1477897d7d0f88452104db732f8fb67a4e
Parker Mayer <thisisseriousbizniz@gmail.com>
2024-02-20 19:07:24 -0800

    Commenting for initial demo

--- ArmCommands/ArmCommands.ino
+++ ArmCommands/ArmCommands.ino
@@ -1,33 +1,96 @@
 #include <AFMotor.h>
 #include <Servo.h>
 
+// Setting up two stepper motors and a servo
 AF_Stepper m1(200,1);
 AF_Stepper m2(200,2);
+Servo myservo;
 
 /*
 * Parker Mayer
-* https://www.marginallyclever.com/2013/08/how-to-build-an-2-axis-arduino-cnc-gcode-interpreter/
+* Junior Design II
+* Block 2 Demo
+* Description: This program receives a command via a serial connection from a Python script.
+               (Either a new GCODE command, a utensil switch command, or a shape draw command.)
+               The program first interprets the incoming command to see whether it's GCode, an MCode,
+               or a shape.
+               - If it's a GCode movement command, it uses a line algorithm to move the stepper motors appropriately.
+               - If it's a utensil-switch MCode command, it moves the servo corresponding to the utensil switch mechanism.
+               - If it's a shape to draw, the corresponding macro executes.
+* NOTE: For GCODE interpretation, I used this article as my main reference:
+*   https://www.marginallyclever.com/2013/08/how-to-build-an-2-axis-arduino-cnc-gcode-interpreter/
 */
 
-// Variables
+// Global variables
 #define BAUD (115200) // Speed of Arduino comms
 #define MAX_BUFF (64) // Max message length sent to Arduino
 
 char buffer[MAX_BUFF]; // where we store the message until we get a ;
 int sofar;             // How much is in the buffer 
 
-#define VERSION (1.0)
-#define MIN_FEEDRATE (50.0)
+#define VERSION (1.0) // Program version
+#define MIN_FEEDRATE (50.0) // Min, max feedrate (need to update, this is just for testing)
 #define MAX_FEEDRATE (100.0)
-float fr = 100.0;
-float nfr = 50.0;
-int mode_abs=1;
-float px = 0.0;
+float fr = 100.0;           // Current feedrate
+float nfr = 50.0;           // New feedrate
+int mode_abs=0;             // Whether the current mode is absolute or relative
+float px = 0.0;             // x, y position
 float py = 0.0;
-long step_delay=1000000.0/fr;
+long step_delay=1000000.0/fr; // Delay between step commands
+int current = 1;            // Current utensil--by default, 1 - pen
 
-String readString = "";
 
+// Start up code to run only once
+void setup(){
+
+  myservo.attach(9); // Set up the servo pin
+  myservo.write(84); // This is the uncalibrated stop PWM for the servo
+  m1.setSpeed(10);   // Motor set up, determine initial speed
+  m2.setSpeed(10);
+  m1.release();
+  m2.release();
+
+  Serial.begin(BAUD);   // Open the serial connection
+  Serial.setTimeout(1); // Set timeout
+
+  setFeedrate(100); // set default speed
+  signalReady();    // Clear the buffer for new input
+}
+
+// Code that loops repeatedly
+void loop(){
+
+  // Listen for commands
+  while(!Serial.available()) {} // Wait for data to arrive
+  if(Serial.available()){       // If something is available
+
+    // Read the incoming string and send it back
+    // to the Python script
+    String test = Serial.readString();
+    Serial.print(test);
+
+    // Check for shapes!
+    if(test.equals("SQUARE")){
+      drawSquare(); // Execute macro
+    } // TO DO: add more shapes....
+
+    // Add each character of the string to the buffer
+    for(int j = 0; j < test.length(); j++){
+      char c = test[j];
+      // IF there's room in the buffer, store the byte.
+      // this prevents loss of data if the buffer is full.
+      if(sofar < MAX_BUFF) buffer[sofar++]=c;
+      // IF we got a return character (\n) then the message is done
+      if(c=='\n'){
+        // Serial.print(F("\r\n")); // for debugging
+        // strings gotta end with a \0
+        buffer[sofar]=0;
+        processCommand(); // DO SOMETHING with the command
+        signalReady();    // Clear the buffer for a new command
+      }
+    }
+  }
+}
 
 /**
  * Look for character /code/ in the buffer and read the float that immediately follows it.
@@ -36,78 +99,18 @@ String readString = "";
  * @input val the return value if /code/ is not found.
  **/
 float parsenumber(char code,float val) {
-  char *ptr=buffer;  // start at the beginning of buffer
+  char *ptr=buffer;  // Start at the beginning of buffer
   while((long)ptr > 1 && (*ptr) && (long)ptr < (long)buffer+sofar) {  // walk to the end
-    if(*ptr==code) {  // if you find code on your walk,
-      return atof(ptr+1);  // convert the digits that follow into a float and return it
+    if(*ptr==code) {  // If you find code on your walk,
+      return atof(ptr+1);  // Convert the digits that follow into a float and return it
     }
-    ptr=strchr(ptr,' ')+1;  // take a step from here to the letter after the next space
+    ptr=strchr(ptr,' ')+1;  // Take a step from here to the letter after the next space
   }
-  return val;  // end reached, nothing found, return default val.
+  return val;  // End reached, nothing found, return default val.
 }
 
-
-// Start up code to run only once
-void setup(){
-
-  m1.setSpeed(10);
-  m2.setSpeed(10);
-
-  Serial.begin(BAUD);
-  Serial.setTimeout(1); 
-
-  //displayMenu(); // display menu
-  setFeedrate(100); // set default speed
-  signalReady();
-}
-
-// loop() runs forever
-void loop(){
-  // Listen for commands
-  while(!Serial.available()) {} // wait for data to arrive
-  if(Serial.available()){ // If something is available
-    // char c = Serial.read(); // Grab the first character
-    // Serial.print(c); // Print what was received
-
-    String test = Serial.readString();
-    Serial.print(test);
-
-    for(int j = 0; j < test.length(); j++){
-      char c = test[j];
-      //IF there's room in the buffer, store the byte.
-      //this prevents loss of data if the buffer is full.
-      if(sofar < MAX_BUFF) buffer[sofar++]=c;
-      // IF we got a return character (\n) then the message is done
-      if(c=='\n'){
-        // Serial.print(F("\r\n")); // for debugging
-        // strings gotta end with a \0
-        buffer[sofar]=0;
-        processCommand(); // DO SOMETHING with the command
-        // signalReady();
-      }
-    }
-  }
-}
-
-/*Display menu info*/
-void displayMenu(){
-
-  Serial.print(F("2 Axis Scara Arm ")); // F stores string in prog mem instead of RAM
-  Serial.println(VERSION);
-  Serial.println(F("COMMANDS"));
-  Serial.println(F("Rapid Positioning: G00 X{x_coord} Y{y_coord}"));
-  Serial.println(F("Linear Interpolation: G01 X{x_coord} Y{y_coord} F{speed}"));
-  Serial.println(F("Absolute Positioning: G90"));
-  Serial.println(F("Relative Positioning: G91"));
-  Serial.println(F("Set Units to Inches: G20"));
-  Serial.println(F("Set Units to Millimeters: G21"));
-  Serial.println(F("End the Program: M02"));
-  Serial.println(F("Change Writing Utensil: M06-{tool_#}"));
-  Serial.println(F("Save Current State: M70"));
-  Serial.println(F("Restore Saved State: M72"));
-}
-
-/*Prepares input buffer to receive new message;
+/*
+  Prepares input buffer to receive new message;
   tells serial connected device it is ready for more
 */
 void signalReady(){
@@ -120,7 +123,6 @@ void signalReady(){
 
 /* Read input buffer and find any recognized commands. One G or M command per line. */
 void processCommand(){
-  // Serial.print("We're in processCommand");
   // Look for commands that start with a "G"
   int cmd=parsenumber('G',-1);
   switch(cmd) {
@@ -129,8 +131,13 @@ void processCommand(){
     // G01
     case 1: // move in a line
       setFeedrate(parsenumber('F', fr));
-      // calling line and passing whether mode is absolute
-      line( parsenumber('X', (mode_abs?px:0) +  (mode_abs?0:px)), parsenumber('Y', (mode_abs?py:0) +  (mode_abs?0:py)) );
+      // calling line and passing whether mode is absolute or not
+      line( parsenumber('X', (mode_abs?px:0) + (mode_abs?0:px)), parsenumber('Y', (mode_abs?py:0) +  (mode_abs?0:py)) );
+      // Afterwards, print current x, y positions
+      Serial.print("Current x position: ");
+      Serial.print(px);
+      Serial.print(" Current y position: ");
+      Serial.print(py);
       break;
 
     // G90 - absolute mode
@@ -147,11 +154,15 @@ void processCommand(){
   // Look for commands that start with an "M"
   cmd=parsenumber('M',-1);
   switch(cmd){
-
     // TO DO: M02
     // TO DO: M06
     // TO DO: M70
     // TO DO: M72
+    case 6: // Switch utensil
+      // Determine whether pen, pencil, or crayon via T-code
+      int whichUtensil = parsenumber('T',-1);
+      servoMove(whichUtensil);
+      break;
     case 18:
       // release motor 1
       m1.release();
@@ -159,54 +170,65 @@ void processCommand(){
       m2.release();
       Serial.print("Release the motors!");
       break;
-    default: break;
+    default: 
+      break;
   }
-
   // If string has no G or M commands, it will reach this place
   // and the Arduino will ignore it....
 }
 
+// Macro for drawing a square...feeds coordinates to line function.
+void drawSquare(){
+  line(100, 200);
+  line(200, 200);
+  line(200, 100);
+  line(100, 100);
+  // Theoretically, positions should be the same after completing the square
+  Serial.print("Current x position: ");
+  Serial.print(px);
+  Serial.print(" Current y position: ");
+  Serial.print(py);
+}
+
 // MOVEMENT ALGORITHM
 
-/*Using Bresenham's line algorithm to move both motors 
+/*
+  Using Bresenham's line algorithm to move both motors 
   @input newx the destination x position
   @input newy the destination y position
+
+  NOTE: Need to port to Inverse Kinematics for SCARA arm
 */
 void line(float newx, float newy){
-
-
-  long dx=newx-px;
+  long dx=newx-px;      // x, y positional difference
   long dy=newy-py;
   int dirx=dx > 0?1:-1; // direction to move
   int diry=dy > 0?1:-1; 
-  dx=abs(dx);
+  dx=abs(dx);           // abs value macro
   dy=abs(dy);
-
   long i;
-
   long over=0;
-
+  // If x difference is greater than y
   if(dx > dy){
     for(i=0; i<dx; i++){
-      // m1 takes one step in dirx
+      // m1 takes steps in dirx
       m1.onestep(dirx, SINGLE);
       over+=dy;
-
+      // If y difference is > x difference
       if(over>=dx){
         over-=dx;
-        // m2 takes one step in diry
+        // m2 takes steps in diry
         m2.onestep(diry, SINGLE);
       }
       pause(step_delay); // step_delay is a global connected to feed rate
-    }
-
-    
+    }    
     // TO DO: test limits here
   } else {
 
     for(i=0; i<dy; ++i){
       // m2 takes one step in diry
       m2.onestep(diry, SINGLE);
+      // If x difference is > y difference
       over+=dx;
       if(over>=dy){
         over-=dy;
@@ -220,14 +242,10 @@ void line(float newx, float newy){
   // Update logical position
   px+=dx*dirx;
   py+=dy*diry;
-
-  Serial.print("Current x position: ");
-  Serial.print(px);
-  Serial.print(" Current y position: ");
-  Serial.print(py);
 }
 
-/* delay for # of microseconds
+/* 
+   Delay for # of microseconds
    @input how many milliseconds to wait
 */
 void pause(long ms){
@@ -235,7 +253,8 @@ void pause(long ms){
   delayMicroseconds(ms%1000);
 }
 
-/* Setting the feedrate (speed motors will move) 
+/* 
+   Setting the feedrate (speed motors will move) 
    @input nfr the new speed in steps/second
 */
 void setFeedrate(float nfr){
@@ -251,4 +270,85 @@ void setFeedrate(float nfr){
   step_delay=1000000.0/nfr;
   fr=nfr;
 }
+
+/* 
+   Deciding how to make the servo move based on current utensil
+   and requested new utensil.
+   @input uten is the code for utensil (1, 2, or 3)
+*/
+void servoMove(int uten){
+
+  switch(current){
+    // If starting from utensil 1 - pen
+    case 1:
+        if(uten==1){
+          myservo.write(84); // this is the uncalibrated stop
+
+        } else if(uten==2){
+          myservo.write(180); // 
+          delay(500);
+          myservo.write(84); // this is the uncalibrated stop
+
+        } else if(uten==3){
+          myservo.write(0); // 
+          delay(500);
+          myservo.write(84); // this is the uncalibrated stop
+        }
+        break;
+    // If starting from utensil 2 - pencil
+    case 2:
+      if(uten==1){
+        myservo.write(90); // 
+        delay(500);
+        myservo.write(84); // this is the uncalibrated stop
+
+      } else if(uten==2){
+        myservo.write(84); // this is the uncalibrated stop
+
+      } else if(uten==3){
+        myservo.write(0); //
+        delay(500);
+        myservo.write(84); // this is the uncalibrated stop
+      }
+      break;
+    // If starting from utensil 3 - crayon
+    case 3:
+      if(uten==1){
+        myservo.write(90); //
+        delay(500);
+        myservo.write(84); // this is the uncalibrated stop
+
+      } else if(uten==2){
+        myservo.write(180); //
+        delay(500);
+        myservo.write(84); // this is the uncalibrated stop
+
+      } else if(uten==3){
+        myservo.write(84); // this is the uncalibrated stop
+      }
+        default: break;
+      }
+
+  current = uten;
+  // Send the current utensil to the Python script
+  Serial.print("Current utensil: " );
+  Serial.print(current);
+}
+
+/*Display menu info -- ONLY FOR TESTING WITHOUT SERIAL CONNECTION */
+// void displayMenu(){
+//   Serial.print(F("2 Axis Scara Arm ")); // F stores string in prog mem instead of RAM
+//   Serial.println(VERSION);
+//   Serial.println(F("COMMANDS"));
+//   Serial.println(F("Rapid Positioning: G00 X{x_coord} Y{y_coord}"));
+//   Serial.println(F("Linear Interpolation: G01 X{x_coord} Y{y_coord} F{speed}"));
+//   Serial.println(F("Absolute Positioning: G90"));
+//   Serial.println(F("Relative Positioning: G91"));
+//   Serial.println(F("Set Units to Inches: G20"));
+//   Serial.println(F("Set Units to Millimeters: G21"));
+//   Serial.println(F("End the Program: M02"));
+//   Serial.println(F("Change Writing Utensil: M06-{tool_#}"));
+//   Serial.println(F("Save Current State: M70"));
+//   Serial.println(F("Restore Saved State: M72"));
+// }
 
--- UserInAnalysis.py
+++ UserInAnalysis.py
@@ -1,30 +1,88 @@
 # Importing Libraries 
 import serial 
 import time 
-# Setting up the duino!
-arduino = serial.Serial(port='COM7', baudrate=115200, timeout=.1)
 
-# boot up time
+'''
+Parker Mayer
+Junior Design II
+Block 2 Demo
+Description: This program establishes a serial connection with
+			 the Arduino board and then sends three types of
+			 user input: a sample GCODE command, a sample
+			 writing utensil selection, and a sample shape
+			 drawing request.
+
+			 After the Arduino processes the commands, it sends
+			 back the state of either the arm motors or the servo.
+'''
+
+
+# Setting up the Arduino serial connection!
+arduino = serial.Serial(port='COM6', baudrate=115200, timeout=.1)
+
+# Give the connection time to establish
 time.sleep(5)
 
 # # # PART 1: TEST GCODE COMMAND # # #
 
-# Write the drawing selection to a file
+# Read the sample GCode command from a text file
 gcode_file = open('SCARA-Arm-Command-Analysis/TEST_GCODE.txt', 'r')
 command = gcode_file.readline()
 gcode_file.close()
+# Append new line for processing by Arduino code
 command+="\n"
-# Serial write section
+# Write command to the serial port
 print("Python value sent: ")
 print(command)
 arduino.write(bytes(command, 'utf-8'))
-time.sleep(1) # with the port open, the response will be buffered 
-msg = arduino.readline() # read everything in the input buffer
+time.sleep(1)  
+# Establish that Arduino received message
+msg = arduino.readline()
 print ("Message from arduino: ")
 print (msg)
+# After Arduino processes command, read status
 time.sleep(0.5)
 print(arduino.readline())
 
+time.sleep(2)
+
 # # # PART 2: UTENSIL SWITCH TEST # # # 
-	
-	
+
+# Read the sample utensil switch command from a text file
+status_file = open('SCARA-Arm-Command-Analysis/TEST_UTENSIL.txt', 'r')
+command = status_file.readline()
+status_file.close()
+command+="\n"
+# Write command to the serial port
+print("Python value sent: ")
+print(command)
+arduino.write(bytes(command, 'utf-8'))
+time.sleep(1)
+# Establish that Arduino received message
+msg = arduino.readline()
+print ("Message from arduino: ")
+print (msg)
+# After Arduino processes command, read status
+time.sleep(0.5)
+print(arduino.readline())
+
+time.sleep(2)
+
+# # # PART 3: SHAPE DRAWING # # # 
+
+# Read the sample shape draw command from a text file
+shape_file = open('SCARA-Arm-Command-Analysis/TEST_SHAPE.txt', 'r')
+command = shape_file.readline()
+shape_file.close()
+# Write command to the serial port
+print("Python value sent: ")
+print(command)
+arduino.write(bytes(command, 'utf-8'))
+time.sleep(1)
+# Establish that Arduino received message
+msg = arduino.readline()
+print ("Message from arduino: ")
+print (msg)
+# After Arduino processes command, read status
+time.sleep(8)
+print(arduino.readline())
